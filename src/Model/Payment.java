030504a821e9e2417529268ea6f582352eee9f9f
Fadhils26 <101819224+Fadhils26@users.noreply.github.com>
2023-01-09 10:42:13 +0700

    Payment

---
+++ src/Model/Payment.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author fadhi
+ */
+public class Payment {
+    private int paymentId, serviceId, harga;
+
+    public Payment(int paymentId, int serviceId, int harga) {
+        this.paymentId = paymentId;
+        this.serviceId = serviceId;
+        this.harga = harga;
+    }
+
+    public int getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(int paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public int getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(int serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+}
